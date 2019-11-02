4d73a232fe05cd2aaf9a124cb1af591fa753d2a0
michalmelis <michalmelis.sk@gmail.com>
2019-11-02 20:47:50 +0100

    Done.

---
+++ open-lab-02.7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace open_lab_02._7
+{
+    class Program
+    {
+
+        public static bool IsTimeForMilkAndCookies(int year, int month, int day)
+        {
+            return month == 12 && day == 24;
+        }
+
+    }
+}
